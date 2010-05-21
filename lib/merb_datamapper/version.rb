c09a074fcfcfd4e416553b07f9acbb9491890b0e
Jonathan Stott <jonathan.stott@gmail.com>
2010-05-21 11:49:13 +0100

    Use the highest DM we can.
    This is in some ways less than ideal, but we want to let people continue
    to use 0.10.x if they have to.  Obviously ideally people would upgrade
    to 1.x quickly.
    
    While this loose dep does have some risk, regarding DM 2.0, we only use
    a very small amount of DM's functionality. It's only from the public (and
    widely used) API and is unlikely to change between versions.

--- lib/merb_datamapper/version.rb
+++ lib/merb_datamapper/version.rb
@@ -1,6 +1,6 @@
 module Merb
   module DataMapper
     VERSION                = '1.1.0'.freeze
-    DM_VERSION_REQUIREMENT = '~> 0.10'.freeze
+    DM_VERSION_REQUIREMENT = '>= 0.10'.freeze
   end
 end
